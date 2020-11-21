dec867cff5328cef0a7d9c574274896e94e546fd
JJ Gray <git@jjgry.com>
2020-11-21 00:14:22 +0000

    adding secret santa

---
+++ projects/secret-santa/santa.py
@@ -0,0 +1,48 @@
+import random
+import urllib.parse
+
+
+def choose_receiver(giver, receivers):
+    viable_receivers = receivers.copy()
+    if giver in viable_receivers:
+        viable_receivers.remove(giver)
+
+    if len(viable_receivers) == 0:
+        return (False, giver)
+    else:
+        return (True, random.choice(viable_receivers))
+
+
+def make_pairs(names):
+    givers = names.copy()
+    receivers = givers.copy()
+
+    pairs = []
+    for giver in givers:
+        (is_successful, receiver) = choose_receiver(giver, receivers)
+
+        if is_successful:
+            pairs.append((giver, receiver))
+        else:
+            return make_pairs(names)
+    return pairs
+
+
+def create_webpages(pairs):
+    with open('./template.shtml') as template:
+        text = template.read()
+
+    for (giver, receiver) in pairs:
+        if giver == 'index':
+            raise Exception("'{}' is not an acceptable name".format(giver))
+        giver_safe = urllib.parse.quote(giver).lower()
+        new_filepath = './{}.shtml'.format(giver_safe)
+
+        modified_text = text.replace('RECEIVER', receiver)
+
+        with open(new_filepath, 'w') as new_file:
+            new_file.write(modified_text)
+
+names = ['Balint', 'Benjamin', 'Izzy', 'JJ', 'Sid']
+pairs = make_pairs(names)
+create_webpages(pairs)
